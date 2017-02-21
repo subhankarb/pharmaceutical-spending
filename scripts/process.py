dca2bc278627933e67ed6d6db56f6d7fad29b5c4
subhankar <subhankar.biswas@target.com>
2017-02-22 04:51:11 +0530

    [xs] Change generate_total_spending.py to scripts/process.py -refs #5

--- scripts/process.py
+++ scripts/process.py
@@ -8,7 +8,7 @@ SPENDING_TOTAL_USD = "pharmaceutical_total_spending.csv"
 
 def load_population_data():
     population_data = {}
-    with open(os.path.join("backup_data", POPULATION_DATA), 'rb') as csv_file:
+    with open(os.path.join("../raw_data", POPULATION_DATA), 'rb') as csv_file:
         population_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
         next(population_reader, None)
         for line in population_reader:
@@ -22,7 +22,7 @@ def load_population_data():
 
 def load_spending_data():
     spending_data = {}
-    with open(os.path.join("backup_data", SPENDING_DATA), 'rb') as csv_file:
+    with open(os.path.join("../raw_data", SPENDING_DATA), 'rb') as csv_file:
         spending_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
         next(spending_reader, None)
         for line in spending_reader:
@@ -35,7 +35,7 @@ def load_spending_data():
 
 def generate_spending_data(population):
     final_data = []
-    with open(os.path.join("backup_data", SPENDING_DATA), 'rb') as csv_file:
+    with open(os.path.join("../raw_data", SPENDING_DATA), 'rb') as csv_file:
         spending_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
         header = next(spending_reader)
         header.append('"TotalValue"')
@@ -54,7 +54,7 @@ def generate_spending_data(population):
                 else:
                     line.append('0')
                 final_data.append(",".join(line))
-    with open(os.path.join("data", SPENDING_TOTAL_USD), 'wt') as final_file:
+    with open(os.path.join("../data", SPENDING_TOTAL_USD), 'wt') as final_file:
         for l in final_data:
             final_file.write(l.replace('"', '') + '\n')
 
